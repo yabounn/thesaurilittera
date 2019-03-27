88b397670abffb4a562bd2f574a73232960b3fc4
Yann Bcd <bcd.yann@gmail.com>
2019-03-27 17:36:40 +0100

    Modification méthode index() dans HomeController + utilisation des annotations dans HomeController pour les routes plutôt que yaml dans routes.yaml

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,20 +4,27 @@ namespace App\Controller;
 
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-class HomeController
+class HomeController extends AbstractController
 {
-    /**
-     * @var Environment
-     */
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
+    // /**
+    //  * @var Environment
+    //  */
+    // public function __construct(Environment $twig)
+    // {
+    //     $this->twig = $twig;
+    // }
 
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
     public function index()
     {
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig');
+        // return new Response($this->twig->render('pages/home.html.twig'));
     }
 }
