d862936ccd0849f28df6b90ee629fab56a39f52c
Yann Bcd <bcd.yann@gmail.com>
2019-04-06 18:18:16 +0200

    Ajout du carousel dans la homepage

---
+++ public/js/carousel.js
@@ -0,0 +1,20 @@
+// function test() {
+//     alert('Hello, comment vas-tu ?');
+// }
+
+// Show three cards in the slide
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  
+    for (var i=0;i<3;i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
